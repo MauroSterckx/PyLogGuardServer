5f8f7096fdcfdb1577cb41d7dbcc337db1e2ccef
MauroSterckx <maurosterckx@gmail.com>
2023-10-30 13:29:11 +0100

    Add Basemodel

--- PyLogGuardClient.py
+++ PyLogGuardClient.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Log(BaseModel):
+    date : str
+    device : str
+    type : str
+    msg : str
+
+    
+    def __init__(self, date: str, device: str,type : str, msg: str, **data):
+        super().__init__(date=date, device=device, type=type, msg=msg, **data)
+
+
 
 @app.get("/")
 def read_root():
@@ -9,4 +22,3 @@ def read_root():
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
-    conn.close()
